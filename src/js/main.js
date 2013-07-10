a4de1f3e52e816f552a923a09dbd1697b59f3ffd
Nicholas Valbusa <info@squallstar.it>
2013-07-10 17:46:50 +0100

    ganalytics commented out (for now)

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
+/*
+//GOOGLE ANALYTICS
 var _gaq=[['_setAccount','UA-39043398-1'],['_trackPageview']];
 (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
 g.src=('https:'==location.protocol?'//ssl':'http://www')+'.google-analytics.com/ga.js';
-s.parentNode.insertBefore(g,s)}(document,'script'));
+s.parentNode.insertBefore(g,s)}(document,'script'));*/
